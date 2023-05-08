841e41dd79e9161b62802ef97d391ae8f3f3d372
William <wifryo@gmail.com>
2023-05-08 17:27:22 +0200

    Initialise project

---
+++ src/main/java/wifryo/spacechraders/SpacechradersApplication.java
@@ -0,0 +1,13 @@
+package wifryo.spacechraders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacechradersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacechradersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wifryo/spacechraders/SpacechradersApplicationTests.java
@@ -0,0 +1,13 @@
+package wifryo.spacechraders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacechradersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
